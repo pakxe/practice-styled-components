b54cf710325397318970c9d1528f73d17752dc37
pakxe <pigkill40@naver.com>
2022-10-20 03:32:53 +0900

    write: 예제2 테마props에 함수 전달하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+
+const Button = styled.button`
+  /* 버튼의 기본적인 스타일링이다. */
+  margin: 1em;
+  padding: 10px;
+  border-radius: 3px;
+
+  /* 테마에 접근해서 만드는 스타일링이다. */
+  color: ${(props) => props.theme.main};
+  border: 2px solid ${(props) => props.theme.main};
+  background: ${(props) => props.theme.back};
+`;
+
+// 테마 객체를 만든다. main과 back이라는 이름의 색을 갖고 있다.
+const theme = {
+  main: "palevioletred",
+  back: "white",
+};
+
+const invertTheme = ({ main, back }) => ({
+  /* 아래의 코드 의미: 이 함수는 새로운 객체를 생성해서 반환하는데 생성하는 객체에는 main, back 이라는 
+  두개의 프로퍼티 키가 존재한다. 그리고 각 프로퍼티 키의 값은 부모 테마에게서 받아온 테마 객체 내부의
+  프로퍼티 main, back의 값인 "white", "palevioletred" 라는 뜻이다. 
+  
+  결국 invertTheme의 반환값은
+  { main: "white", back: "palevioletred" } 가 된다. */
+  main: back,
+  back: main,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Button>원래의 테마</Button>
+      <ThemeProvider theme={invertTheme}>
+        <Button>바뀐 테마</Button>
+      </ThemeProvider>
+    </ThemeProvider>
   );
 }
 
